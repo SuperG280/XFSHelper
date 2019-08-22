29bfed490fb1bfc1da8d6636a72164abca88775a
Ramon Suarez Alvarez <rsalvarez@gmail.com>
2019-08-22 20:29:09 +0200

    Incluida animación al clicar sobre la imagen en Acerca de.

--- app/src/main/java/com/superg280/dev/xfshelper/AboutActivity.java
+++ app/src/main/java/com/superg280/dev/xfshelper/AboutActivity.java
@@ -1,7 +1,12 @@
 package com.superg280.dev.xfshelper;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        final ImageView icon = findViewById(R.id.imageView_about);
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Animation rotateAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim);
+                icon.startAnimation(rotateAnimation);
+            }
+        });
+
     }
 }
